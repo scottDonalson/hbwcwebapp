eee87b00bc0dc8d79d50ebd1e7afcadd03023ffe
Scott Donalson <scott.donalson@okstate.edu>
2025-02-23 00:55:35 -0600

    new frontend Dockerfile

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,11 +3,10 @@ import "../styles/globals.css";
 import Navbar from "@/Components/NavBar/nav_bar";
 import Footer from "@/Components/Footer/footer";
 import GoogleAnalytics from "@/Components/GoogleAnalytics/GoogleAnalytics";
-
 import localFont from 'next/font/local';
 
 const inter = localFont({
-    src: '/fonts/Inter-Regular.woff2', // Must be relative to "public"
+    src: '../../public/fonts/Inter-Regular.woff2',
     weight: '400',
     style: 'normal',
     display: 'swap',
